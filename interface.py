861f9952dabc6ba58e6f4f013282fec714eb538a
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2024-04-18 01:22:50 -0400

    Modify generation hyperparameters

--- interface.py
+++ interface.py
@@ -11,9 +11,6 @@ default_hyperparameters = {
     "temperature": 1,
     "max_tokens": 400,
     "top_p": 1,
-    "frequency_penalty": 0.0,
-    "presence_penalty": 0.0,
-    "request_timeout": 45
 }
 
 QUESTION_BANK = {
